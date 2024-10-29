96b70f3f33d16047523f99739f2840f84fec028d
Hamza <khanzadahamza76>
2024-10-29 04:00:34 -0700

    my-portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  
+  theme: {
+    extend: {
+      colors: {
+        blacks: "#1f1f1f",
+        green: "#28e98c",
+        gray: "#999999",
+      },
+    },
+  },
+  plugins: [],
+}
